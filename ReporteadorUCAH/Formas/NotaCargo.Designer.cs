dc30ae97da47ccb142a4ac7b4baed6aad75c0d7c
Omar Pineda <c.omar_pinedae@hotmail.com>
2025-09-13 21:54:25 -0700

    Borde en el modelo del form

--- ReporteadorUCAH/Formas/NotaCargo.Designer.cs
+++ ReporteadorUCAH/Formas/NotaCargo.Designer.cs
@@ -28,178 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            label1 = new Label();
-            dateTimePicker1 = new DateTimePicker();
-            label2 = new Label();
-            label3 = new Label();
-            textBox2 = new TextBox();
-            button1 = new Button();
+            panel1 = new Panel();
+            button3 = new Button();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
             button2 = new Button();
             label4 = new Label();
             textBox3 = new TextBox();
-            label5 = new Label();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            label6 = new Label();
-            textBox6 = new TextBox();
-            label7 = new Label();
-            button3 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // textBox1
+            // panel1
             // 
-            textBox1.Location = new Point(61, 43);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 49);
-            label1.Name = "label1";
-            label1.Size = new Size(43, 15);
-            label1.TabIndex = 4;
-            label1.Text = "# Nota";
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(354, 43);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(96, 23);
-            dateTimePicker1.TabIndex = 5;
-            // 
-            // label2
-            // 
-            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            label2.AutoSize = true;
-            label2.Location = new Point(310, 46);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 6;
-            label2.Text = "Fecha";
-            // 
-            // label3
-            // 
-            label3.Anchor = AnchorStyles.Left;
-            label3.AutoSize = true;
-            label3.Location = new Point(20, 103);
-            label3.Name = "label3";
-            label3.Size = new Size(44, 15);
-            label3.TabIndex = 8;
-            label3.Text = "Cliente";
-            // 
-            // textBox2
-            // 
-            textBox2.Anchor = AnchorStyles.Left;
-            textBox2.Location = new Point(71, 100);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(307, 23);
-            textBox2.TabIndex = 7;
-            // 
-            // button1
-            // 
-            button1.Anchor = AnchorStyles.Left;
-            button1.BackgroundImage = Properties.Resources.icons8_encuentra_hombre_usuario_32;
-            button1.BackgroundImageLayout = ImageLayout.Stretch;
-            button1.Location = new Point(384, 95);
-            button1.Name = "button1";
-            button1.Size = new Size(33, 31);
-            button1.TabIndex = 9;
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Anchor = AnchorStyles.Left;
-            button2.BackgroundImage = Properties.Resources.icons8_brote_32;
-            button2.BackgroundImageLayout = ImageLayout.Stretch;
-            button2.Location = new Point(384, 124);
-            button2.Name = "button2";
-            button2.Size = new Size(33, 31);
-            button2.TabIndex = 12;
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            label4.Anchor = AnchorStyles.Left;
-            label4.AutoSize = true;
-            label4.Location = new Point(20, 132);
-            label4.Name = "label4";
-            label4.Size = new Size(45, 15);
-            label4.TabIndex = 11;
-            label4.Text = "Cultivo";
-            // 
-            // textBox3
-            // 
-            textBox3.Anchor = AnchorStyles.Left;
-            textBox3.Location = new Point(71, 129);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(307, 23);
-            textBox3.TabIndex = 10;
-            // 
-            // label5
-            // 
-            label5.Anchor = AnchorStyles.Left;
-            label5.AutoSize = true;
-            label5.Location = new Point(6, 161);
-            label5.Name = "label5";
-            label5.Size = new Size(59, 15);
-            label5.TabIndex = 14;
-            label5.Text = "Toneladas";
-            // 
-            // textBox4
-            // 
-            textBox4.Anchor = AnchorStyles.Left;
-            textBox4.Location = new Point(71, 158);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(108, 23);
-            textBox4.TabIndex = 15;
-            textBox4.Text = "0";
-            textBox4.TextAlign = HorizontalAlignment.Right;
-            // 
-            // textBox5
-            // 
-            textBox5.Anchor = AnchorStyles.Left;
-            textBox5.Location = new Point(270, 158);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(108, 23);
-            textBox5.TabIndex = 17;
-            textBox5.Text = "0";
-            textBox5.TextAlign = HorizontalAlignment.Right;
-            // 
-            // label6
-            // 
-            label6.Anchor = AnchorStyles.Left;
-            label6.AutoSize = true;
-            label6.Location = new Point(224, 161);
-            label6.Name = "label6";
-            label6.Size = new Size(40, 15);
-            label6.TabIndex = 16;
-            label6.Text = "Precio";
-            // 
-            // textBox6
-            // 
-            textBox6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            textBox6.BackColor = SystemColors.Info;
-            textBox6.Location = new Point(342, 240);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(108, 23);
-            textBox6.TabIndex = 19;
-            textBox6.Text = "0";
-            textBox6.TextAlign = HorizontalAlignment.Right;
-            // 
-            // label7
-            // 
-            label7.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            label7.AutoSize = true;
-            label7.Location = new Point(287, 243);
-            label7.Name = "label7";
-            label7.Size = new Size(49, 15);
-            label7.TabIndex = 18;
-            label7.Text = "Importe";
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 32);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(457, 235);
+            panel1.TabIndex = 5;
             // 
             // button3
             // 
@@ -210,77 +84,201 @@
             button3.FlatStyle = FlatStyle.Flat;
             button3.Image = Properties.Resources.icons8_efectivo_20;
             button3.ImageAlign = ContentAlignment.MiddleRight;
-            button3.Location = new Point(342, 209);
+            button3.Location = new Point(342, 173);
             button3.Name = "button3";
             button3.Size = new Size(108, 25);
-            button3.TabIndex = 20;
+            button3.TabIndex = 54;
             button3.Text = "Deducciones";
             button3.TextAlign = ContentAlignment.MiddleLeft;
             button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            textBox6.BackColor = SystemColors.Info;
+            textBox6.Location = new Point(342, 204);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(108, 23);
+            textBox6.TabIndex = 53;
+            textBox6.Text = "0";
+            textBox6.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Location = new Point(287, 207);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 15);
+            label7.TabIndex = 52;
+            label7.Text = "Importe";
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Left;
+            textBox5.Location = new Point(270, 122);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(108, 23);
+            textBox5.TabIndex = 51;
+            textBox5.Text = "0";
+            textBox5.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Left;
+            label6.AutoSize = true;
+            label6.Location = new Point(224, 125);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 15);
+            label6.TabIndex = 50;
+            label6.Text = "Precio";
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Left;
+            textBox4.Location = new Point(71, 122);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(108, 23);
+            textBox4.TabIndex = 49;
+            textBox4.Text = "0";
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Left;
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 125);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 15);
+            label5.TabIndex = 48;
+            label5.Text = "Toneladas";
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Left;
+            button2.BackgroundImage = Properties.Resources.icons8_brote_32;
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.Location = new Point(384, 88);
+            button2.Name = "button2";
+            button2.Size = new Size(33, 31);
+            button2.TabIndex = 47;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Left;
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 96);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 15);
+            label4.TabIndex = 46;
+            label4.Text = "Cultivo";
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Left;
+            textBox3.Location = new Point(71, 93);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(307, 23);
+            textBox3.TabIndex = 45;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Left;
+            button1.BackgroundImage = Properties.Resources.icons8_encuentra_hombre_usuario_32;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(384, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(33, 31);
+            button1.TabIndex = 44;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Left;
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 43;
+            label3.Text = "Cliente";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Left;
+            textBox2.Location = new Point(71, 64);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(307, 23);
+            textBox2.TabIndex = 42;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(310, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 41;
+            label2.Text = "Fecha";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(354, 7);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(96, 23);
+            dateTimePicker1.TabIndex = 40;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 39;
+            label1.Text = "# Nota";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(61, 7);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 38;
             // 
             // NotaCargo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(463, 302);
-            Controls.Add(button3);
-            Controls.Add(textBox6);
-            Controls.Add(label7);
-            Controls.Add(textBox5);
-            Controls.Add(label6);
-            Controls.Add(textBox4);
-            Controls.Add(label5);
-            Controls.Add(button2);
-            Controls.Add(label4);
-            Controls.Add(textBox3);
-            Controls.Add(button1);
-            Controls.Add(label3);
-            Controls.Add(textBox2);
-            Controls.Add(label2);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
+            Controls.Add(panel1);
             Name = "NotaCargo";
             Text = "Nota cargo";
-            Controls.SetChildIndex(textBox1, 0);
-            Controls.SetChildIndex(label1, 0);
-            Controls.SetChildIndex(dateTimePicker1, 0);
-            Controls.SetChildIndex(label2, 0);
-            Controls.SetChildIndex(textBox2, 0);
-            Controls.SetChildIndex(label3, 0);
-            Controls.SetChildIndex(button1, 0);
-            Controls.SetChildIndex(textBox3, 0);
-            Controls.SetChildIndex(label4, 0);
-            Controls.SetChildIndex(button2, 0);
-            Controls.SetChildIndex(label5, 0);
-            Controls.SetChildIndex(textBox4, 0);
-            Controls.SetChildIndex(label6, 0);
-            Controls.SetChildIndex(textBox5, 0);
-            Controls.SetChildIndex(label7, 0);
-            Controls.SetChildIndex(textBox6, 0);
-            Controls.SetChildIndex(button3, 0);
+            Controls.SetChildIndex(panel1, 0);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private Label label1;
-        private DateTimePicker dateTimePicker1;
-        private Label label2;
-        private Label label3;
-        private TextBox textBox2;
-        private Button button1;
+        private Panel panel1;
+        private Button button3;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
         private Button button2;
         private Label label4;
         private TextBox textBox3;
-        private Label label5;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private Label label6;
-        private TextBox textBox6;
-        private Label label7;
-        private Button button3;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private TextBox textBox1;
     }
 }
