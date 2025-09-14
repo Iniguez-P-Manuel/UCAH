555e4c9d50ee7f16a5d948a15e0a1aad18f117bf
Omar Pineda <c.omar_pinedae@hotmail.com>
2025-09-13 21:35:28 -0700

    Interfaz v1

---
+++ ReporteadorUCAH/Formas/NotaCargo.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace ReporteadorUCAH.Formas
+{
+    partial class NotaCargo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(61, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 4;
+            label1.Text = "# Nota";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(354, 43);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(96, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(310, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Fecha";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Left;
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Cliente";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Left;
+            textBox2.Location = new Point(71, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(307, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Left;
+            button1.BackgroundImage = Properties.Resources.icons8_encuentra_hombre_usuario_32;
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(384, 95);
+            button1.Name = "button1";
+            button1.Size = new Size(33, 31);
+            button1.TabIndex = 9;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Left;
+            button2.BackgroundImage = Properties.Resources.icons8_brote_32;
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.Location = new Point(384, 124);
+            button2.Name = "button2";
+            button2.Size = new Size(33, 31);
+            button2.TabIndex = 12;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Left;
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Cultivo";
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Left;
+            textBox3.Location = new Point(71, 129);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(307, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Left;
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 161);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 15);
+            label5.TabIndex = 14;
+            label5.Text = "Toneladas";
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Left;
+            textBox4.Location = new Point(71, 158);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(108, 23);
+            textBox4.TabIndex = 15;
+            textBox4.Text = "0";
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Left;
+            textBox5.Location = new Point(270, 158);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(108, 23);
+            textBox5.TabIndex = 17;
+            textBox5.Text = "0";
+            textBox5.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Left;
+            label6.AutoSize = true;
+            label6.Location = new Point(224, 161);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Precio";
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            textBox6.BackColor = SystemColors.Info;
+            textBox6.Location = new Point(342, 240);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(108, 23);
+            textBox6.TabIndex = 19;
+            textBox6.Text = "0";
+            textBox6.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Location = new Point(287, 243);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Importe";
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button3.BackColor = Color.DarkSeaGreen;
+            button3.BackgroundImageLayout = ImageLayout.Stretch;
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 255, 128);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Image = Properties.Resources.icons8_efectivo_20;
+            button3.ImageAlign = ContentAlignment.MiddleRight;
+            button3.Location = new Point(342, 209);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 25);
+            button3.TabIndex = 20;
+            button3.Text = "Deducciones";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // NotaCargo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(463, 302);
+            Controls.Add(button3);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "NotaCargo";
+            Text = "Nota cargo";
+            Controls.SetChildIndex(textBox1, 0);
+            Controls.SetChildIndex(label1, 0);
+            Controls.SetChildIndex(dateTimePicker1, 0);
+            Controls.SetChildIndex(label2, 0);
+            Controls.SetChildIndex(textBox2, 0);
+            Controls.SetChildIndex(label3, 0);
+            Controls.SetChildIndex(button1, 0);
+            Controls.SetChildIndex(textBox3, 0);
+            Controls.SetChildIndex(label4, 0);
+            Controls.SetChildIndex(button2, 0);
+            Controls.SetChildIndex(label5, 0);
+            Controls.SetChildIndex(textBox4, 0);
+            Controls.SetChildIndex(label6, 0);
+            Controls.SetChildIndex(textBox5, 0);
+            Controls.SetChildIndex(label7, 0);
+            Controls.SetChildIndex(textBox6, 0);
+            Controls.SetChildIndex(button3, 0);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private Button button3;
+    }
+}
--- ReporteadorUCAH/Main.Designer.cs
+++ ReporteadorUCAH/Main.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GestionPracticas
+﻿namespace ReporteadorUCAH
 {
     partial class Main
     {
@@ -30,76 +30,100 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             panel1 = new Panel();
+            btnNotasCargo = new Button();
             panel2 = new Panel();
             panel3 = new Panel();
-            button7 = new Button();
+            button3 = new Button();
             button2 = new Button();
             btnCerrar = new Button();
-            Titulo = new Label();
             PanelVentana = new Panel();
             button1 = new Button();
+            btnClientes = new Button();
+            btnCultivos = new Button();
             panel1.SuspendLayout();
             panel3.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(55, 71, 79);
+            panel1.BackColor = Color.PeachPuff;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(btnCultivos);
+            panel1.Controls.Add(btnClientes);
+            panel1.Controls.Add(btnNotasCargo);
             panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 0);
+            panel1.Location = new Point(0, 29);
             panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(175, 612);
+            panel1.Size = new Size(175, 583);
             panel1.TabIndex = 0;
+            // 
+            // btnNotasCargo
+            // 
+            btnNotasCargo.BackColor = Color.Tan;
+            btnNotasCargo.Dock = DockStyle.Top;
+            btnNotasCargo.FlatStyle = FlatStyle.Flat;
+            btnNotasCargo.Image = Properties.Resources.icons8_documento_20;
+            btnNotasCargo.ImageAlign = ContentAlignment.MiddleRight;
+            btnNotasCargo.Location = new Point(0, 60);
+            btnNotasCargo.Name = "btnNotasCargo";
+            btnNotasCargo.Size = new Size(171, 36);
+            btnNotasCargo.TabIndex = 2;
+            btnNotasCargo.Text = "Notas cargo";
+            btnNotasCargo.UseVisualStyleBackColor = false;
+            btnNotasCargo.Click += button4_Click;
             // 
             // panel2
             // 
-            panel2.BackColor = Color.FromArgb(38, 50, 56);
+            panel2.BackColor = Color.PeachPuff;
+            panel2.BackgroundImage = Properties.Resources.UCAH_LOGO_trans_2;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
             panel2.Margin = new Padding(2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(175, 60);
+            panel2.Size = new Size(171, 60);
             panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            panel3.BackColor = Color.FromArgb(69, 90, 100);
-            panel3.Controls.Add(button7);
+            panel3.BackColor = Color.Peru;
+            panel3.Controls.Add(button3);
             panel3.Controls.Add(button2);
             panel3.Controls.Add(btnCerrar);
-            panel3.Controls.Add(Titulo);
             panel3.Dock = DockStyle.Top;
-            panel3.Location = new Point(175, 0);
+            panel3.Location = new Point(0, 0);
             panel3.Margin = new Padding(2);
             panel3.Name = "panel3";
-            panel3.Size = new Size(695, 60);
+            panel3.Size = new Size(870, 29);
             panel3.TabIndex = 1;
             panel3.MouseMove += panel3_MouseMove;
             // 
-            // button7
+            // button3
             // 
-            button7.Dock = DockStyle.Left;
-            button7.FlatAppearance.BorderSize = 0;
-            button7.FlatStyle = FlatStyle.Flat;
-            button7.Location = new Point(0, 0);
-            button7.Margin = new Padding(2);
-            button7.Name = "button7";
-            button7.Size = new Size(65, 60);
-            button7.TabIndex = 3;
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click;
+            button3.Dock = DockStyle.Right;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Image = Properties.Resources.icons8_línea_horizontal_16;
+            button3.Location = new Point(774, 0);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(32, 29);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
             button2.Dock = DockStyle.Right;
             button2.FlatAppearance.BorderSize = 0;
             button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(565, 0);
+            button2.Image = Properties.Resources.icons8_squares_16_fliped;
+            button2.Location = new Point(806, 0);
             button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(65, 60);
+            button2.Size = new Size(32, 29);
             button2.TabIndex = 2;
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click_1;
@@ -109,39 +133,26 @@
             btnCerrar.Dock = DockStyle.Right;
             btnCerrar.FlatAppearance.BorderSize = 0;
             btnCerrar.FlatStyle = FlatStyle.Flat;
-            btnCerrar.Location = new Point(630, 0);
+            btnCerrar.Image = Properties.Resources.icons8_eliminar_16;
+            btnCerrar.Location = new Point(838, 0);
             btnCerrar.Margin = new Padding(2);
             btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(65, 60);
+            btnCerrar.Size = new Size(32, 29);
             btnCerrar.TabIndex = 1;
             btnCerrar.UseVisualStyleBackColor = true;
             btnCerrar.Click += button1_Click;
             // 
-            // Titulo
-            // 
-            Titulo.AutoSize = true;
-            Titulo.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Titulo.ForeColor = SystemColors.ButtonHighlight;
-            Titulo.Location = new Point(5, 12);
-            Titulo.Margin = new Padding(2, 0, 2, 0);
-            Titulo.MinimumSize = new Size(665, 30);
-            Titulo.Name = "Titulo";
-            Titulo.Size = new Size(665, 30);
-            Titulo.TabIndex = 0;
-            Titulo.Text = "Home";
-            Titulo.TextAlign = ContentAlignment.MiddleCenter;
-            Titulo.MouseMove += Titulo_MouseMove;
-            // 
             // PanelVentana
             // 
             PanelVentana.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            PanelVentana.BackgroundImageLayout = ImageLayout.Center;
+            PanelVentana.BackColor = Color.LightGray;
+            PanelVentana.BackgroundImageLayout = ImageLayout.Stretch;
             PanelVentana.Dock = DockStyle.Fill;
             PanelVentana.ForeColor = SystemColors.ControlText;
-            PanelVentana.Location = new Point(175, 60);
+            PanelVentana.Location = new Point(175, 29);
             PanelVentana.Margin = new Padding(2);
             PanelVentana.Name = "PanelVentana";
-            PanelVentana.Size = new Size(695, 552);
+            PanelVentana.Size = new Size(695, 583);
             PanelVentana.TabIndex = 2;
             // 
             // button1
@@ -151,7 +162,36 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 64);
             button1.TabIndex = 1;
+            button1.Text = "asdasdasd";
             button1.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            btnClientes.BackColor = Color.Tan;
+            btnClientes.Dock = DockStyle.Top;
+            btnClientes.FlatStyle = FlatStyle.Flat;
+            btnClientes.Image = Properties.Resources.icons8_encuentra_hombre_usuario_23;
+            btnClientes.ImageAlign = ContentAlignment.MiddleRight;
+            btnClientes.Location = new Point(0, 96);
+            btnClientes.Name = "btnClientes";
+            btnClientes.Size = new Size(171, 36);
+            btnClientes.TabIndex = 3;
+            btnClientes.Text = "Clientes";
+            btnClientes.UseVisualStyleBackColor = false;
+            // 
+            // btnCultivos
+            // 
+            btnCultivos.BackColor = Color.Tan;
+            btnCultivos.Dock = DockStyle.Top;
+            btnCultivos.FlatStyle = FlatStyle.Flat;
+            btnCultivos.Image = Properties.Resources.icons8_brote_23;
+            btnCultivos.ImageAlign = ContentAlignment.MiddleRight;
+            btnCultivos.Location = new Point(0, 132);
+            btnCultivos.Name = "btnCultivos";
+            btnCultivos.Size = new Size(171, 36);
+            btnCultivos.TabIndex = 4;
+            btnCultivos.Text = "Cultivos";
+            btnCultivos.UseVisualStyleBackColor = false;
             // 
             // Main
             // 
@@ -159,16 +199,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(870, 612);
             Controls.Add(PanelVentana);
-            Controls.Add(panel3);
             Controls.Add(panel1);
+            Controls.Add(panel3);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            IsMdiContainer = true;
             Margin = new Padding(2);
             Name = "Main";
             Text = "Form1";
+            WindowState = FormWindowState.Maximized;
             panel1.ResumeLayout(false);
             panel3.ResumeLayout(false);
-            panel3.PerformLayout();
             ResumeLayout(false);
 
         }
@@ -179,11 +220,13 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel PanelVentana;
-        private System.Windows.Forms.Label Titulo;
         private System.Windows.Forms.Button btnCerrar;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button7;
+        private Button button3;
+        private Button btnNotasCargo;
+        private Button btnClientes;
+        private Button btnCultivos;
     }
 }
 
